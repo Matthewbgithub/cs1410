776c6c7c1bd4af4233d168184977f5cdbf8ae0a7
HannahMiller98 <hannah.louhm@googlemail.com>
2017-03-13 16:17:41 +0000

    Update Sedan.java
    this is sedans

--- src/cs1410/Sedan.java
+++ src/cs1410/Sedan.java
@@ -2,5 +2,30 @@ package cs1410;
 
 public class Sedan extends Vehicle{
 
+	private  double probabiltyOfQ;
+	private int shoppingTime;
+	private int shoppingMoney;
+	
+	public Vehicle(probabilityOfQ, shoppingTime, shoppingMoney){
+	this.probabilityofQ = probabiltyOfQ;
+	this.shoppingTime = shoppingTime;
+	this.shoppingMoney = shoppingMoney;
+	}
+	
+	public void setProbabilityOfQ(double probabilityOfQ){
+	this.probabilityofQ = probabiltyOfQ;
+	
+	}
+	public double getProbabilityOfQ(){
+	return probabilityOfQ;
+	}
+	
+	public void setShoppingTime(int shoppingTime){
+	this.shoppingTime = shoppingTime;	
+	}
+	
+	public int getShoppingTime(){
+	return shoppingTime;
+	}
 	
 }
