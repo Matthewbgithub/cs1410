2883f741cdfebead1602f37492628ece655ee284
Matt <Matthewbarlow98@gmail.com>
2017-03-13 15:10:18 +0000

    small changes

--- src/cs1410/Motorbike.java
+++ src/cs1410/Motorbike.java
@@ -1,6 +1,7 @@
 package cs1410;
 
 public class Motorbike extends Vehicle{
+	
 	public Motorbike(){
 		probability = 0;
 	}
--- src/cs1410/Sedan.java
+++ src/cs1410/Sedan.java
@@ -2,4 +2,5 @@ package cs1410;
 
 public class Sedan extends Vehicle{
 
+	
 }
