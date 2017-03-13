db02a2312b9d183993d669516fde22b3fdd34e98
forfuxksake <gopaljir@aston.ac.uk>
2017-03-13 15:54:12 +0000

    Update Truck.java
    stuff

--- src/cs1410/Truck.java
+++ src/cs1410/Truck.java
@@ -1,5 +1,16 @@
 package cs1410;
+import java.util.Random;
 
 public class Truck extends Vehicle{
+	private double probabilityOfT;
+	private double shoppingTime;
+	private double shoppingMoney;
+	private Random random;
+	
+public Truck() {
+	int high = 6;
+	int low = 4;
+	shoppingTime = random.nextDouble(high - low) + low;
+}
 	
 }
