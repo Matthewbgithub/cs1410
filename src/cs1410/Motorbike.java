7c27583aa43883cfbf5289172ca9ab1c8bf77e7e
forfuxksake <gopaljir@aston.ac.uk>
2017-03-13 15:45:14 +0000

    Update Motorbike.java

--- src/cs1410/Motorbike.java
+++ src/cs1410/Motorbike.java
@@ -1,8 +1,18 @@
 package cs1410;
 
 public class Motorbike extends Vehicle{
+	private double probabilityOfP;
+	private double shoppingProbability;
 	
 	public Motorbike(){
-		probability = 0;
+		shoppingProbability = 0;
+	}
+	
+	private void setProbabilityOfP(double probability) {
+		probabilityOfP = probability;
+	}
+	
+	private double getProbabilityOfP() {
+		return probabilityOfP;	
 	}
 }
