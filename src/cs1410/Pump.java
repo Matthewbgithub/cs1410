f0ffea0305df445bc50c3c704a10037142a01965
DontAskForMyName <nkosi_0121@hotmail.co.uk>
2017-03-13 16:23:36 +0000

    Update Pump.java

--- src/cs1410/Pump.java
+++ src/cs1410/Pump.java
@@ -1,5 +1,12 @@
 package cs1410;
+import java.util.Random
 
-public class Pump {
-	private int pumpSize = 3;
-}
+
+public class Pump
+	
+	// This is to decide which vehicle comes into the queue (between 0 and 1)
+	private Random rndVehicleDecider;
+	
+		{
+
+		}
