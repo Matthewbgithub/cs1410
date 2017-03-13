7b0ff171052d5f044c7e342a27ce023b30b8b787
forfuxksake <gopaljir@aston.ac.uk>
2017-03-13 16:06:21 +0000

    Update Truck.java

--- src/cs1410/Truck.java
+++ src/cs1410/Truck.java
@@ -6,11 +6,41 @@ public class Truck extends Vehicle{
 	private double shoppingTime;
 	private double shoppingMoney;
 	private Random random;
+	private int refillTime;
+	private boolean isHappy;
 	
 public Truck() {
-	int high = 6;
-	int low = 4;
-	shoppingTime = random.nextDouble(high - low) + low;
+	int timeHigh = 6;
+	int timeLow = 4;
+	shoppingTime = random.nextDouble(timeHigh - timeLow) + timeLow;
+	
+	int moneyHigh = 20;
+	int moneyLow = 15;
+	shoppingMoney = random.nextDouble(moneyHigh - moneyLow) + moneyLow;
 }
 	
+	private void setProbabilityOfT(double probability) {
+		probabilityOfT = probability;	
+	}
+	
+	private double getProbabilityOfT() {
+		return probabilityOfT;
+	}
+	
+	private void setRefillTime(int time) {
+		refillTime = time;
+	}
+	
+	private int getRefillTime() {
+		return refillTime;
+	}
+	
+	private boolean isHappy() {
+		if(refillTime > 8) {
+			return true;
+		}
+		else {
+			return false;
+		}
+	}
 }
