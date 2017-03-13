3263f5d264c6c2427e7e87563c5c82d03d10cce9
Matt <Matthewbarlow98@gmail.com>
2017-03-13 16:09:43 +0000

    changed queue

--- src/cs1410/Queue.java
+++ src/cs1410/Queue.java
@@ -1,5 +1,24 @@
 package cs1410;
 
 public class Queue {
-	
+	private int qLength;
+	public void setLength(int length){
+		qLength = length;
+	}
+	public int getLength(){
+		return qLength;
+	}
+	public boolean checkspace(int sizeToAdd){
+		//checks whether the space passed as a parameter will fit into the queue
+		boolean result = true;
+		return result;
+	}
+	public void removeFirstItem(){
+		//removes first item from array
+		//changes space left in queue
+	}
+	public int getCurrentLength(){
+		//will calulate and return the length of all the items in the queue
+		return 1;
+	}
 }
