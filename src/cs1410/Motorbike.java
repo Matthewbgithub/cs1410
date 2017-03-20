5be9211a160503573e9cf96b1415a8bf53c2d78b
HannahMiller98 <hannah.louhm@googlemail.com>
2017-03-20 15:53:28 +0000

    Update Motorbike.java

--- src/cs1410/Motorbike.java
+++ src/cs1410/Motorbike.java
@@ -1,8 +1,19 @@
+/**
+* Represents the Motobike class which is a subclass of Vehicle
+* @author
+* @version`
+*/
+
 package cs1410;
 
 public class Motorbike extends Vehicle{
 	private double probToAppearP;
-	
+	/**
+	* Creates a <code> Motorbike </code> 
+	* Sets the tank size, shopping probability, shopping money, space in queue and shopping time
+	* 
+	*/
+		
 	public Motorbike(){
 		tankSize = 5;
 		shoppingProb = 0;
@@ -13,11 +24,19 @@ public class Motorbike extends Vehicle{
 	}
 	
 	//set arrival probability
+	/**
+	* @param sets the probability of the appearance of the motorbike using a double
+	*/
+		
 	private void setProbabilityOfP(double probability) {
 		probToAppearP = probability;
 	}
 	
 	//get arrival probability
+	/**
+	* @return probability set by user
+	
+	*/
 	private double getProbabilityOfP() {
 		return probToAppearP;	
 	}
