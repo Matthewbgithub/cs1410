ee88458b84a0941970b10812300a86e230d2acbb
BeckyH98 <beckyholmes98@hotmail.co.uk>
2017-03-20 14:54:13 +0000

    Update Car.java

--- src/cs1410/Car.java
+++ src/cs1410/Car.java
@@ -3,17 +3,16 @@ import java.util.Random;
 
 public class Car extends Vehicle{
 	private double probToAppearP;
-	private Random random;
 	public Car(){
 		qSpace = 1;
 		random = new Random();
-		tankSize = random.nextInt(3)+7;
+		tankSize = rnd.nextInt(3)+7;
 		refillTime = tankSize / 6;
 		
 		if(refillTime < 5) {
 			shoppingProb = 0.3;
-			shoppingTime = random.nextInt(3)+2;
-			shoppingMoney = random.nextDouble(9)+8;
+			shoppingTime = rnd.nextInt(3)+2;
+			shoppingMoney = rnd.nextDouble(6)+5;
 	}
 	private void setProbabilityOfP(double probability) {
 		probToAppearP = probability;
