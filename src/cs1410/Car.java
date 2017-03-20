3fcb28260f8b5da2d8c91794c653225dbb56b85f
BeckyH98 <beckyholmes98@hotmail.co.uk>
2017-03-20 15:06:00 +0000

    Update Car.java

--- src/cs1410/Car.java
+++ src/cs1410/Car.java
@@ -9,15 +9,18 @@ public class Car extends Vehicle{
 		tankSize = rnd.nextInt(3)+7;
 		refillTime = tankSize / 6;
 		
+		//if the refill time is less than 5 minutes, the probability of shopping is 0.3
 		if(refillTime < 5) {
 			shoppingProb = 0.3;
 			shoppingTime = rnd.nextInt(3)+2;
 			shoppingMoney = rnd.nextDouble(6)+5;
 	}
+		//set arrival probability
 	private void setProbabilityOfP(double probability) {
 		probToAppearP = probability;
 	}
 	
+		//get arrival probability
 	private double getProbabilityOfP() {
 		return probToAppearP;	
 	}
