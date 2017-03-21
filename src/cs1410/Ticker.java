af964c0396483ea5098895c6508be82f1adac0ba
BeckyH98 <beckyholmes98@hotmail.co.uk>
2017-03-21 13:22:09 +0000

    added some methods

--- src/cs1410/Ticker.java
+++ src/cs1410/Ticker.java
@@ -2,4 +2,22 @@ package cs1410;
 
 public class Ticker {
 
+  	private int tick;
+	
+	public Ticker() {
+		tick = 0;
+	}
+	
+	public int getTick() {
+		return tick;
+	}
+	
+	public void increment() {
+		tick++;
+	}
+	
+	public void reset() {
+		tick = 0;
+	}
+  
 }
