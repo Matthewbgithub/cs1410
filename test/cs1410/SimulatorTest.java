f76bb69d1199de4440ae1f99b87a8c6317d319c2
forfuxksake <gopaljir@aston.ac.uk>
2017-03-21 13:12:49 +0000

    simulatorTest

---
+++ test/cs1410/SimulatorTest.java
@@ -0,0 +1,19 @@
+package cs1410;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class SimulatorTest {
+
+	@Test
+	public void testMain() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetTicks() {
+		fail("Not yet implemented");
+	}
+
+}
