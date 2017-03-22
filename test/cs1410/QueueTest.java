285683729cb9d14daeff20c44a7648ed383b0000
HannahMiller98 <hannah.louhm@googlemail.com>
2017-03-22 15:37:47 +0000

    continued testing queue

--- test/cs1410/QueueTest.java
+++ test/cs1410/QueueTest.java
@@ -10,27 +10,42 @@ public class QueueTest{
 	
 	ArrayList<Vehicle>vehicleArray = new ArrayList<Vehicle>();
 	Queue queue = new Queue();
+	private final int qLength=3;
+	private double currentLength=0.0;
 	
 	Car c1 = new Car();
 	Car c2 = new Car();
 	Car c3 = new Car();
+	Motorbike m1 = new Motorbike();
+	Motorbike m2 = new Motorbike();
+	Sedan s1 = new Sedan();
+	Sedan s2 = new Sedan();
+	Truck t1 = new Truck();
 
-	@Test
-	public void testQueue() {
-		
-	}
+	
 
 	@Test
 	public void testGetCurrentLength() {
 		vehicleArray.add(c1);
 		vehicleArray.add(c2);
 		vehicleArray.add(c3);
-		assertEquals(3.0, vehicleArray.size());
+		assertEquals(3, vehicleArray.size());
+		
+		vehicleArray.add(m1);
+		vehicleArray.add(s1);
+		vehicleArray.add(s2);
+		assertEquals(3, qLength);
+			
 	}
 
 	@Test
 	public void testCheckspace() {
-		fail("Not yet implemented");
+		vehicleArray.add(c1);
+		vehicleArray.add(c2);
+		vehicleArray.add(c3);
+		currentLength = 3;
+		vehicleArray.add(m1);
+		assertTrue(false , Queue.checkSpace());
 	}
 
 	@Test
@@ -43,9 +58,6 @@ public class QueueTest{
 		fail("Not yet implemented");
 	}
 
-	@Test
-	public void testToString() {
-		fail("Not yet implemented");
-	}
+	
 
 }
