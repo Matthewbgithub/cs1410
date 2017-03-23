1e1b91674beab1180516f67c8ffac413b8b8e660
forfuxksake <gopaljir@aston.ac.uk>
2017-03-23 23:59:53 +0000

    pump test attempt
    testAdd method in pump test fails but logic is there I believe

--- test/cs1410/PumpTest.java
+++ test/cs1410/PumpTest.java
@@ -10,15 +10,19 @@ public class PumpTest {
 
 	ArrayList<Vehicle>vehicleArray = new ArrayList<Vehicle>();
 	Queue pumpQueue = new Queue();
+	private final int qLength=3;
+	private double currentLength=0.0;
 	
 	Sedan s1 = new Sedan();
 	Sedan s2 = new Sedan();
+	Sedan s3 = new Sedan();
 	
 	@Test
 	public void testAdd() {
-		vehicleArray.add(s1);
-		vehicleArray.add(s2);
-		assertTrue(true, vehicleArray.size());
+		pumpQueue.add(s1, null);
+		//pumpQueue.add(s2, null);
+		//currentLength = 3.0;
+		assertEquals(s1, pumpQueue);
 	}
 
 
